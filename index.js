a10a7b441bb814a1e31587fec74c699830204c5b
Sitorakhon <sitorakhon_yuldasheva@student.itpu.uz>
2026-04-10 15:02:36 +0500

    dfhbvbfvh

---
+++ index.js
@@ -0,0 +1,170 @@
+// console.log("Salom");
+
+// Javascriptda 3 ta ozgaruvchilar bor
+
+// let
+// var
+// const
+
+
+// let ism = 'Zakariyo'
+// ism = 'GUZAL'
+// var fam = "Palon"
+// fam = "PIs"
+// const yosh = 13
+// yosh = 23
+
+// console.log(ism);
+
+// Arifmetik operatorlar
+
+// let s = 20
+// let a =" 20"
+
+
+// console.log(s + a);
+// console.log(s - a);
+// console.log(s * a);
+// console.log(s / a);
+// console.log(s ** a);
+// console.log(s % a);
+
+
+// tenglik operator
+
+// console.log(s === a);
+// console.log(s == a);
+
+// Data type
+
+// String --- "Salom"
+// Number --- 1253274
+// Boolean --- true false
+// undefined ----- undefined
+// null --- null
+
+
+
+
+
+
+
+// let price = 1000
+// for (i = 12; i <= 20; i++) {
+
+//     let son =i/10
+//     console.log(son+ "Kanfet narxi" + (price  * son) + "som");
+
+// }
+
+
+
+
+// let a = 2
+// let b = 10
+// let nat = 0
+
+// for(i = a;i <= b;i++){
+//           console.log(i);
+//           nat++
+          
+// }
+// console.log(nat);
+
+
+
+
+
+
+// let str = "Sal om "
+// console.log(str.length);
+
+
+
+// String length
+// String concat()
+// String slice()
+// String substring()
+// String substr()
+// String toUpperCase()
+// String toLowerCase()
+// String trim()
+// String trimStart()
+// String trimEnd()
+// String repeat()
+// String replace()
+// String replaceAll()
+// String split()
+// .reverse().join("");
+// .includes()
+
+
+
+
+
+// const inson = {
+//           name:"Kamronbek",
+//           fam:"bvhjbv",
+//           yosh:15,
+//           hobby:["Foodball","uxlash","dangasa"],
+//           manzil:{
+//                     vil:"Andijon",
+//                     tum:"Qorgon",
+//                     shax:"Qorgontepa",
+//                     mfy:"Oltinvodiy",
+//                     kocha:"Sayhun",
+//                     uy:31
+//           },
+//           sinf: 9,
+
+// }
+// console.log(inson.name);
+
+
+// const talaba = {
+//   ism: "talabacha",
+//   baho1: 90,
+//   baho2: 60,
+//   baho3: 100
+// };
+
+// function ortachaBaho(obj) {
+//   let bahobek = (obj.baho1 + obj.baho2 + obj.baho3) / 3;
+//   return bahobek;
+// }
+
+// console.log(ortachaBaho(talaba));
+
+
+// const max = {
+//           nomi:"telefon",
+//           narx:100000
+// }
+
+
+// let son = 0
+
+
+if(son > 1){
+       console.log(`Son katta`);
+          
+}
+else{
+          console.log(`Son kichik`);
+          
+}
+
+let yosh = 15
+
+if(yosh === 16){
+          console.log(`Pasport oladi`);
+          
+}
+else if(yosh > 16){
+          console.log(`Pasport olgan`);
+          
+} 
+else {
+          console.log(`Yosh kichik`);
+          
+}
